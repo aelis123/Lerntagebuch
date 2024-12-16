8218ed9e3edef21229cc6f12abef0a4284e15bba
aelis123 <gehring.alice@web.de>
2024-12-16 10:58:32 +0100

    Neue Handbucheinträge

--- Lernseite2/src/views/ToDoView.vue
+++ Lernseite2/src/views/ToDoView.vue
@@ -35,7 +35,7 @@
     <!-- Archivierte To-Dos -->
     <div class="todo-archive">
       <h3 @click="toggleArchive">
-        📦 Archiv {{ archiveOpen ? '⯆' : '⯈' }} ({{ archivedTodos.length }})
+        📦 Archiv {{ archiveOpen ? '👇' : '👉' }} ({{ archivedTodos.length }})
       </h3>
       <ul v-show="archiveOpen">
         <li
